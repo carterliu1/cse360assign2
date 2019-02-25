8f99329fdf39adacff64a6bb8e01d6d8c41567f8
carterliu <carterliu@192.168.6.51>
2019-02-25 12:18:45 -0700

    Creating calculator repository

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,63 @@
+/*
+*@author : Carter Liu
+*@Class ID : 457
+*@Assignment : 2
+*@Description : This file contains operations for a calculator
+*/
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * This method returns the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * This method performs the add operation
+	 * @param value the number to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * This method performs the subtract operation
+	 * @param value the number to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * This method performs the multiply operation
+	 * @param value the number to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/*
+	 * This method performs the divide operation
+	 * @param value the number to be divided
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/*
+	 * This method returns the history as a string
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
